bb45de542db7c7964f93b80c2097e7177323175e
Niusha H <niusha.hadziomerovic@gmail.com>
2018-07-16 04:48:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,10 @@
-def prime?(i)
-  return false if >= 1
-  prime = i % i  
-end
+def prime?(num)
+    return false if >= 1
+
+    2.upto(Math.sqrt(num).ceil) do |i|
+        break if num%i==0
+        return true if i==Math.sqrt(num).ceil   
+    end
+    return false
+
+endend
